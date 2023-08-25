993bb3f294d632f186882bd168f5069c3473b068
Pedro <paf.victor@gmail.com>
2023-08-25 10:01:52 -0300

    feat: Adicionando cláusula de verificação de resultados

--- resources/views/task/read.blade.php
+++ resources/views/task/read.blade.php
@@ -8,20 +8,25 @@
 <body>
     <h1>READ PAGE</h1>
     <div>
-        <table border="1">  
-            <tr>
-                <th>ID</th>
-                <th>Título</th>
-                <th>Descrição</th>
-            </tr>
-            @foreach ($tasks as $singleTask) <!-- -A variável $tasks (plural) é provida pelo controlador TaskController. Basicamente essa variável contém todos os registros do banco de dados.-->
+        @if (sizeof($tasks) > 0) <!-- Tem registros no banco -->
+            <table border="1">  
                 <tr>
-                    <td>{{$singleTask->id}}</td>
-                    <td>{{$singleTask->titulo}}</td>
-                    <td>{{$singleTask->desc}}</td>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Descrição</th>
                 </tr>
-            @endforeach
-        </table>
+                @foreach ($tasks as $singleTask) <!-- -A variável $tasks (plural) é provida pelo controlador TaskController. Basicamente essa variável contém todos os registros do banco de dados.-->
+                    <tr>
+                        <td>{{$singleTask->id}}</td>
+                        <td>{{$singleTask->titulo}}</td>
+                        <td>{{$singleTask->desc}}</td>
+                    </tr>
+                @endforeach
+            </table>
+        @else
+            <h1>Sem nada no banco</h1>
+        @endif
+
     </div>
 </body>
 </html>
