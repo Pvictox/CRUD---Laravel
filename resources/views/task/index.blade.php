ffed0b72f7cb1225a832c4b398dc6299cbdaf7e2
Pedro <paf.victor@gmail.com>
2023-08-26 08:24:00 -0300

    docs: Modificando Readme

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -8,7 +8,7 @@
 <body>
     <h1>VIEW INDEX</h1>
 
-    <button type="button" onclick="window.location = '{{route('task.create')}}' ">Create</button>
+    <button type="button" onclick="window.location = '{{route('task.create')}}'">Create</button>
     <button type="button" onclick="window.location = '{{route('task.read')}}' ">Mostrar todos</button>
 </body>
 </html>
